f0d9110ee030f3b50e681e11b7a2a81277e48e89
tobitayuuki <flying59@outlook.jp>
2023-02-19 13:57:01 +0900

    20230219 commite

--- VoicevoxAPI/Program.cs
+++ VoicevoxAPI/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VoicevoxAPI;
 
+// Main method
+VoicevoxFunction voicevoxFunction = new("192.168.0.5", "50021");
+
+await voicevoxFunction.MakeSound("C:¥voicevox", "sample", "こんにちは世界", true, 1);
